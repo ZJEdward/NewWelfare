eb6a93f5e75eaf87e1330ff3f737cd4429423179
luhuiyue <luhuiyue@weipaidai.com>
2017-11-15 10:43:16 +0800

    luhuiyue

--- js/index.js
+++ js/index.js
@@ -7,13 +7,27 @@
              * 
              */
             init:function(){
-                console.log("111")
-                this.bindEvent();
+                window.onload = function(){
+                   App.goScrollTop(); 
+                }         
+            },
+            goScrollTop:function(){
+                var fixel = $('.footer');
+                console.log(fixel,$('.fixed-box'))
+                $(window).scroll(function(e){
+                    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;                       
+                    if (scrollTop > 10) {
+                        $('.footer').show(.5);
+                    } else {
+                        $('.footer').hide(.5);
+                    }
+                });
             },
             //事件绑定
             bindEvent:function(){
 
             }
+        }
     }();
     window.addEventListener('DOMContentLoaded', App.init(), false);
-}(window))
+})(window)
