2e7ef06b5d9b57072aa97743f09e8738eb4c16ff
luhuiyue <luhuiyue@weipaidai.com>
2017-11-16 10:56:02 +0800

    luhuiyue

--- js/index.js
+++ js/index.js
@@ -8,24 +8,45 @@
              */
             init:function(){
                 window.onload = function(){
-                   App.goScrollTop(); 
+                    
+                   App.bindEvent(); 
                 }         
             },
-            goScrollTop:function(){
-                var fixel = $('.footer');
-                console.log(fixel,$('.fixed-box'))
-                $(window).scroll(function(e){
-                    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;                       
-                    if (scrollTop > 10) {
-                        $('.footer').show(.5);
-                    } else {
-                        $('.footer').hide(.5);
-                    }
-                });
+            checkPhone:function(phone){
+                var reg = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;                    
+                return reg.test(phone); 
+            },
+            /**
+             * 手机端toast提示
+             * @param {String} msg
+             * @param {String} time
+            */
+            toast:function(msg,time){
+                var time = time || 2000;
+                if($('.util-toast').length >= 1){
+                    return false;
+                }
+                $('body').append('<div class="util-toast">' + msg + '</div>');
+                setTimeout(function() {
+                    $('.util-toast').remove();
+                    $('.fixed-button').removeClass('btn-disable').removeAttr("disabled");
+                }, time);
             },
             //事件绑定
             bindEvent:function(){
-
+                $('.fixed-button').on('click',function(){
+                    var phone = $('.phone-input').val();
+                    $(this).addClass('btn-disable').attr("disabled","true");
+                    if(!phone){
+                        App.toast("请输入手机号码!");
+                        return false;                        
+                    }else if(!App.checkPhone(phone)){
+                        App.toast("手机号码有误，请重填!");                     
+                        return false;
+                    }else{
+                        window.location.href="main.html";                                   
+                    };                    
+                })
             }
         }
     }();
